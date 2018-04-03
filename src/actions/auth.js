dc6c947671cea5688e20110be209a38cc0e6c46a
Hesam <hesam.kamalan@gmail.com>
2018-04-02 18:53:06 -0600

    Social buttons added for login by Google/Facebook.

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,4 +1,4 @@
-import { firebase } from './../firebase'
+import { firebase, googleAuthProvider } from './../firebase'
 
 export const ACTION_LOGIN_BY_EMAIL = "ActionLogInByEmail"
 export const ACTION_LOGOUT = "ActionLogOut"
@@ -11,8 +11,8 @@ export const logoutAction = () => ({
 	type: ACTION_LOGOUT
 })
 
-export const startLoginByEmail = (email, password, nextAction) => {
-	console.log("Firebase signin...")
+export const startLoginByEmail = (email, password, nextAction, onErrorByEmailLogin) => {
+	console.log("Login by email...")
 	firebase.auth().signInWithEmailAndPassword(email, password)
 		.then(() => {
 			console.log("Successful Login!")
@@ -20,7 +20,20 @@ export const startLoginByEmail = (email, password, nextAction) => {
 		})
 		.catch((error) => {
 			console.log("oops, Couldn't login. Message: " + error.message)
+			onErrorByEmailLogin(error.message)
 		})
+}
+
+export const startLoginByGoogle = (nextAction) => {
+	console.log("Login by Google...")
+		return firebase.auth().signInWithPopup(googleAuthProvider)
+			.then(() => {
+				console.log("Successful Login!")
+				nextAction()
+			})
+			.catch((error) => {
+
+			})
 }
 
 export const startLogout = (nextAction) => {
